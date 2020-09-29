e2e68dfaa8c4d44025a59da967c508b9bf60a44e
muzili <2586850402@qq.com>
2020-09-29 16:03:57 +0800

    1.7支持管理员修改按键映射
    管理员在后台插件配置修改页可以修改玩家一玩家二的键盘操作按键映射

--- app.php
+++ app.php
@@ -14,6 +14,7 @@ class KodNESPlugin extends PluginBase{
         // $this->echoFile('static/config.js'); //通过文件输出;支持模板变量
     }
 	public function index() {
+	    $key=$this->getconfig();
 		include($this->pluginPath.'static/page.html');
 	}
 }
